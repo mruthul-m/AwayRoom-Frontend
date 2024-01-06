241ea3cb3c419dfa78d5718bed06f24eff00622e
mruthul-m <memruthu@gmail.com>
2024-01-06 22:42:18 +0530

    listing pages all front-end work has been done

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -1,10 +1,29 @@
-const FormInput = () => {
+interface Props {
+  label: string;
+  onChange: React.FC;
+  id: number;
+  type: string;
+  inputProps: unknown;
+  value: any;
+  errorMessage: string;
+}
+
+const FormInput = (props: Props) => {
+  const { label, errorMessage, onChange, id, type, ...inputProps } = props;
+
   return (
-    <div className="flex flex-col  border-2 border-red-600 ">
-      <label htmlFor="" className="text-left">
-        Name
+    <div className="flex flex-col ">
+      <label htmlFor="" className="text-left text-neutral-500 italic">
+        {label}
       </label>
-      <input className="bg-transparent border-2 border-black rounded-lg p-1 outline-none text-lg w-3/5" />
+      <input
+        {...inputProps}
+        type={type}
+        className="  rounded-lg p-2 outline-none text-lg w-full mb-5 max-h-fit text-black font-normal"
+        style={{ backgroundColor: "rgba(221,230,237,.4)" }}
+        onChange={onChange}
+        name={inputProps.name}
+      />
     </div>
   );
 };
--- src/pages/Listing/AddListing.tsx
+++ src/pages/Listing/AddListing.tsx
@@ -4,16 +4,32 @@ import { HiOutlineThumbUp } from "react-icons/hi";
 import "./addlist.css";
 import FormInput from "../../components/FormInput/FormInput";
 import { useState } from "react";
+
+interface Values {
+  username: string;
+  phone: number | string;
+  address: string;
+  city: string;
+  images: string[];
+  zipcode: number | string;
+  category: string;
+  monthlyrent: number;
+}
 function AddListing() {
-  const [values, setValues] = useState({
-    name: "",
+  const [values, setValues] = useState<Values>({
+    username: "",
     phone: "",
-    address1: "",
+    address: "",
     city: "",
-    address2: "",
     images: [],
     zipcode: "",
+    category: "apartment",
+    monthlyrent: 0,
   });
+
+  {
+    setValues;
+  }
 
   const inputs = [
     {
@@ -23,6 +39,7 @@ function AddListing() {
       placeholder: "Name",
       errorMessage: "",
       label: "Name :",
+      required: true,
     },
     {
       id: 2,
@@ -31,14 +48,16 @@ function AddListing() {
       placeholder: "ph number",
       errorMessage: "",
       label: "Contact No :",
+      required: true,
     },
     {
       id: 3,
-      name: "address1",
-      type: "text",
-      placeholder: "address 1",
+      name: "address",
+      type: "textarea",
+      placeholder: "address",
       errorMessage: "",
-      label: "address 1:",
+      label: "address :",
+      required: true,
     },
     {
       id: 4,
@@ -47,14 +66,17 @@ function AddListing() {
       placeholder: "city",
       errorMessage: "",
       label: "City :",
+      required: true,
     },
     {
       id: 5,
-      name: "address2",
-      type: "text",
-      placeholder: "address 2",
+      name: "images",
+      type: "file",
+      placeholder: "",
       errorMessage: "",
-      label: "Address2 :",
+      label: "images :",
+      multiple: true,
+      required: true,
     },
     {
       id: 6,
@@ -63,8 +85,22 @@ function AddListing() {
       placeholder: "zip code",
       errorMessage: "",
       label: "Zip code :",
+      required: true,
     },
   ];
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(values);
+  };
+
+  const onChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
   return (
     <div className="">
       <div
@@ -72,27 +108,67 @@ function AddListing() {
         className="w-full h-screen py-10"
       >
         <Navbar content={true} />
-        <div className="max-w-screen-xl mx-auto my-5  bg-white/[.8] px-4 py-4">
+        <div className="max-w-screen-xl mx-auto my-5  bg-white/[.8] px-4 py-12">
           <p className="addlist-heading text-5xl" style={{ lineHeight: "1.5" }}>
             Your Asset can be listed to our platform and be part of our family
             <HiOutlineThumbUp className="inline mx-5" />
           </p>
-          <p
-            style={{ fontFamily: "Merienda" }}
-            className="text-4xl font-bold text-black"
+
+          <form
+            action="#"
+            className="bg-white mt-8 py-8 rounded-lg shadow-lg shadow-black  w-3/4 mx-auto"
+            onSubmit={handleSubmit}
           >
-            Details:{" "}
-            <span className="font-semibold text-xl">
-              Here you can give your details and best pictures of your asset to
-              show case. Only best pictures are considering for showcasing.
-            </span>
-          </p>
-          <form action="" className="bg-transparent">
-            <div className="grid grid-cols-2 gap-3 w-full border-2 border-black">
+            {" "}
+            <h1 className="block font-bold text-3xl mb-5">
+              Share your details
+            </h1>
+            <div className="grid grid-cols-2  w-full gap-x-16">
               {inputs.map((input) => (
-                <FormInput key={input.id} />
+                <FormInput
+                  key={input.id}
+                  value={values[input.name as keyof typeof values]}
+                  {...input}
+                  onChange={onChange}
+                />
               ))}
+              <div className="w-full">
+                <h1 className="text-left mb-2 text-neutral-500 italic">
+                  Category
+                </h1>
+                <select
+                  defaultValue="apartment"
+                  name="category"
+                  className="h-10 rounded-xl px-5 w-full text-black font-nor cursor-pointer"
+                  style={{ backgroundColor: "rgba(221,230,237,.4)" }}
+                  onChange={onChange}
+                >
+                  <option value="boys">Boys Room</option>
+                  <option value="apartment">Apartment</option>
+                  <option value="girls">Pg for Girls</option>
+                </select>
+              </div>
+              <div className="w-full">
+                <h1 className="text-left mb-2 text-neutral-500 italic">
+                  Monthly Rent
+                </h1>
+                <input
+                  name="monthlyrent"
+                  type="number"
+                  className="  rounded-lg p-1 outline-none text-lg w-full mb-5 max-h-fit text-black font-normal"
+                  style={{ backgroundColor: "rgba(221,230,237,.4)" }}
+                  onChange={onChange}
+                  required
+                />
+              </div>
             </div>
+            <button
+              // onClick={handleSubmit}
+              type="submit"
+              className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-24 mt-8"
+            >
+              Send
+            </button>
           </form>
         </div>
       </div>
